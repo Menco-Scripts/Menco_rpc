4af5b3cb8d3e199a1ef8b6ca3b07055dd34298cd
KaosTheDev <121107509+KaosTheDev@users.noreply.github.com>
2025-07-05 20:45:29 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,10 +1,10 @@
-local function formatString(template, variables)
+local function fstring(template, variables)
     return (template:gsub("{(.-)}", function(key)
         return tostring(variables[key] or "nil")
     end))
 end
 
-local function getLocalizedString(path)
+local function lang(path)
     local parts = {}
     for part in string.gmatch(path, "[^.]+") do
         table.insert(parts, part)
@@ -22,7 +22,7 @@ local function getLocalizedString(path)
     return tbl
 end
 
-local function getPlayerInfo()
+local function get_player_info()
     local ped = PlayerPedId()
     local coords = GetEntityCoords(ped)
     local playerId = GetPlayerServerId(PlayerId())
@@ -32,7 +32,7 @@ local function getPlayerInfo()
     if streetHash ~= 0 then
         streetName = GetStreetNameFromHashKey(streetHash)
     else
-        local unknownText = getLocalizedString("status.unknown")
+        local unknownText = lang("status.unknown")
         if unknownText then
             streetName = unknownText
         end
@@ -68,49 +68,45 @@ end
 
 local function buildStatusMessage(playerInfo)
     if playerInfo.isInVehicle and playerInfo.vehicleName then
-        local template = getLocalizedString("status.driving") or "Driving a {vehicle} on {street}"
-        return formatString(template, {
+        local template = lang("status.driving") or "Driving a {vehicle} on {street}"
+        return fstring(template, {
             vehicle = playerInfo.vehicleName,
             street = playerInfo.streetName
         })
     else
-        local template = getLocalizedString("status." .. playerInfo.movementState) or "{state} on {street}"
+        local template = lang("status." .. playerInfo.movementState) or "{state} on {street}"
         local stateName = playerInfo.movementState:gsub("^%l", string.upper)
-        return formatString(template, {
+        return fstring(template, {
             state = stateName,
             street = playerInfo.streetName
         })
     end
 end
 
-local function updateDiscordPresence()
-    local playerInfo = getPlayerInfo()
-    local statusMessage = buildStatusMessage(playerInfo)
-    local playerIdText = formatString(getLocalizedString("actions.playerId") or "Player ID: {id}", {
-        id = playerInfo.playerId
-    })
-
-    SetDiscordAppId(Config.DiscordAppId)
-    SetDiscordRichPresenceAsset(Config.Assets.LargeImage)
-    SetDiscordRichPresenceAssetText(Config.Assets.LargeImageText)
-    SetDiscordRichPresenceAssetSmall(Config.Assets.SmallImage)
-    SetDiscordRichPresenceAssetSmallText(Config.Assets.SmallImageText)
-    SetDiscordRichPresenceAction(0, playerIdText, "")
-    SetDiscordRichPresenceAction(1, statusMessage, "")
-    SetRichPresence(("ID %d | %s"):format(playerInfo.playerId, statusMessage))
-end
-
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(Config.UpdateInterval)
-        updateDiscordPresence()
+        local playerInfo = get_player_info()
+        local statusMessage = buildStatusMessage(playerInfo)
+        local playerIdText = fstring(lang("actions.playerId") or "Player ID: {id}", {
+            id = playerInfo.playerId
+        })
+    
+        SetDiscordAppId(Config.DiscordAppId)
+        SetDiscordRichPresenceAsset(Config.Assets.LargeImage)
+        SetDiscordRichPresenceAssetText(Config.Assets.LargeImageText)
+        SetDiscordRichPresenceAssetSmall(Config.Assets.SmallImage)
+        SetDiscordRichPresenceAssetSmallText(Config.Assets.SmallImageText)
+        SetDiscordRichPresenceAction(0, playerIdText, "")
+        SetDiscordRichPresenceAction(1, statusMessage, "")
+        SetRichPresence(("ID %d | %s"):format(playerInfo.playerId, statusMessage))
     end
 end)
 
 RegisterCommand('discord', function()
-    local discordLink = Config.DiscordLink or "https://discord.gg/unknown"
-    local template = getLocalizedString("commands.discordMessage") or "Join our Discord server: {discord}"
-    local msg = formatString(template, { discord = discordLink })
+    local discord_link = Config.DiscordLink or "https://discord.gg/unknown"
+    local template = lang("commands.discordMessage") or "Join our Discord server: {discord}"
+    local msg = fstring(template, { discord = discordLink })
 
     TriggerEvent('chat:addMessage', {
         color = {0, 123, 255},
